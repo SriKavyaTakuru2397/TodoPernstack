c018127cb577173308d57ffaecba9bafa4fae8f1
SriKavyaTakuru23 <srikavya.takuru@raagvitech.com>
2025-08-29 19:20:24 +0530

    noww

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const pool = require('./db');
+const path=require("path");
 
 //middleware
 app.use(cors());
@@ -13,6 +14,12 @@ app.use(express.json());
 // })
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+// app.use(express.static(path.join(__dirname,"../client/build")));
+app.use(express.static("../client/build"));
+if(process.env.NODE_ENV === "production"){
+    app.use(express.static(path.join(__dirname,"../client/build")));
+    console.log(__dirname);
+}
 
 //Build Routes with postgres queries
 app.post("/todos",async(req,res)=>{
